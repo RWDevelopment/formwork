61210c7f472734f1d9a7d4d526a7504026e899a2
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2025-05-27 17:46:51 +0200

    Add new layouts

--- templates/layouts/matcha.php
+++ templates/layouts/matcha.php
@@ -4,7 +4,7 @@
     <title><?= $this->escape($page->title()) ?> | <?= $this->escape($site->title()) ?></title>
     <?= $this->insert('_meta') ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <link rel="stylesheet" href="https://matcha.mizu.sh/matcha.css">
 </head>
 <body>
 <?= $this->insert('_menu') ?>
