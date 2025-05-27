1eb228d203bf0a5d2c839fd6f278843ba165a0a0
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2025-05-27 14:03:31 +0200

    Update post.php

--- templates/post.php
+++ templates/post.php
@@ -1,10 +1,8 @@
 <?= $this->layout('site') ?>
-<main>
-    <div class="container">
-        <article>
-            <h1 class="article-title"><a href="<?= $page->uri() ?>"><?= $this->escape($page->title()) ?></a></h1>
-            <?= $this->insert('_tags', ['post' => $page, 'blog' => $page->parent()]) ?>
-            <?= $page->content() ?>
-        </article>
-    </div>
+<main class="container">
+    <article>
+        <h1><a href="<?= $page->uri() ?>"><?= $this->escape($page->title()) ?></a></h1>
+        <?= $this->insert('_tags', ['post' => $page, 'blog' => $page->parent()]) ?>
+        <?= $page->content() ?>
+    </article>
 </main>
