4bce4625323060e44a23dbcf5e22c06a55ca1185
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2025-05-27 14:04:41 +0200

    Update blog.php

--- templates/blog.php
+++ templates/blog.php
@@ -6,20 +6,18 @@
         </div>
     </aside>
 <?php endif; ?>
-<main>
-    <div class="container">
-        <?php foreach ($posts as $post) : ?>
-            <article>
-                <h1 class="article-title"><a href="<?= $post->uri() ?>"><?= $this->escape($post->title()) ?></a></h1>
-                <?= $this->insert('_tags', ['post' => $post, 'blog' => $page]) ?>
-                <?php if ($post->summary()) : ?>
-                    <?= $post->summary() ?>
-                    <a class="read-more" href="<?= $post->uri() ?>" rel="bookmark">Read more &rarr;</a>
-                <?php else : ?>
-                    <?= $post->content() ?>
-                <?php endif; ?>
-            </article>
-        <?php endforeach; ?>
-    </div>
+<main class="container">
+    <?php foreach ($posts as $post) : ?>
+         <article>
+            <h1><a href="<?= $post->uri() ?>"><?= $this->escape($post->title()) ?></a></h1>
+            <?= $this->insert('_tags', ['post' => $post, 'blog' => $page]) ?>
+            <?php if ($post->summary()) : ?>
+                <?= $post->summary() ?>
+                <a href="<?= $post->uri() ?>" rel="bookmark">Read more &rarr;</a>
+            <?php else : ?>
+                <?= $post->content() ?>
+            <?php endif; ?>
+        </article>
+    <?php endforeach; ?>
 </main>
 <?= $this->insert('_pagination') ?>
