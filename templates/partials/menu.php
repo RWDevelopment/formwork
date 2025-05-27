c89ac2c2ee448dc0f10424fdd76eab3105269b02
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2025-05-27 15:52:34 +0200

    Update menu.php

--- templates/partials/menu.php
+++ templates/partials/menu.php
@@ -1,11 +1,14 @@
-<nav class="menu">
-    <div class="container">
-        <a class="menu-header" href="<?= $site->uri() ?>"><?= $this->escape($site->title()) ?></a>
-        <button type="button" class="menu-toggle" data-toggle="main-menu" aria-expanded="false">&#9776;</button>
-        <div class="menu-list menu-collapse" id="main-menu">
-            <?php foreach ($site->children()->filter('visible') as $item) : ?>
-                <a class="menu-item<?php if ($item->isCurrent()) : ?> active<?php endif; ?>" href="<?= $item->uri() ?>"><?= $this->escape($item->get('menu', $item->title())) ?></a>
-            <?php endforeach; ?>
-        </div>
-    </div>
+<nav class="container">
+    <ul>
+        <li>
+            <a href="<?= $site->uri() ?>"><?= $this->escape($site->title()) ?></a>
+        </li>
+    </ul>
+    <ul>
+        <?php foreach ($site->children()->filter('visible') as $item) : ?>
+            <li>
+                <a class="<?php if ($item->isCurrent()) : ?>active<?php endif; ?>" href="<?= $item->uri() ?>"><?= $this->escape($item->get('menu', $item->title())) ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </nav>
