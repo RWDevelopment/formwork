446de7b115d39062a393fe6c6546fe34bfa0e7a5
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2025-05-27 17:49:31 +0200

    Add new layouts

---
+++ templates/layouts/launch.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="<?= $site->languages()->current() ?>">
+<head>
+    <title><?= $this->escape($page->title()) ?> | <?= $this->escape($site->title()) ?></title>
+    <?= $this->insert('_meta') ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://unpkg.com/launch.css" />
+</head>
+<body>
+<?= $this->insert('_menu') ?>
+<?= $this->content() ?>
+<?= $this->insert('_footer') ?>
+</body>
+</html>
