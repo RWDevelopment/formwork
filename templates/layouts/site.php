2090e3b4bc909472e9735251041297358af69732
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2025-05-27 16:21:57 +0200

    Update site.php

--- templates/layouts/site.php
+++ templates/layouts/site.php
@@ -4,7 +4,7 @@
     <title><?= $this->escape($page->title()) ?> | <?= $this->escape($site->title()) ?></title>
     <?= $this->insert('_meta') ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="<?= $this->assets()->uri('css/style.min.css') ?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     <script src="<?= $this->assets()->uri('js/script.min.js') ?>"></script>
 </head>
 <body>
