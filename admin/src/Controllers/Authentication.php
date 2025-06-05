3011fe7c219b7e4edbdd2343a06f6a4e31ffddbc
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2025-06-05 15:05:59 +0200

    add login user

--- admin/src/Controllers/Authentication.php
+++ admin/src/Controllers/Authentication.php
@@ -88,6 +88,84 @@ class Authentication extends AbstractController
     }
 
     /**
+     * Authentication@login action
+     */
+    public function loginUser(): void
+    {
+        $limiter = new AccessLimiter(
+            $this->registry('accessAttempts'),
+            $this->option('admin.login_attempts'),
+            $this->option('admin.login_reset_time')
+        );
+
+        if ($limiter->hasReachedLimit()) {
+            $minutes = round($this->option('admin.login_reset_time') / 60);
+            $this->error($this->label('login.attempt.too-many', $minutes));
+            return;
+        }
+
+        switch (HTTPRequest::method()) {
+            case 'GET':
+                if (Session::has('FORMWORK_USERNAME')) {
+                    $this->redirectToPanel();
+                    //$this->redirect('/user');
+                }
+
+                // Always generate a new CSRF token
+                CSRFToken::generate();
+
+                $this->view('authentication.login', [
+                    'title' => $this->label('login.login')
+                ]);
+
+                break;
+
+            case 'POST':
+                // Delay request processing for 0.5-1s
+                usleep(random_int(500, 1000) * 1e3);
+
+                $data = new DataGetter(HTTPRequest::postData());
+
+                // Ensure no required data is missing
+                if (!$data->hasMultiple(['username', 'password'])) {
+                    $this->error($this->label('login.attempt.failed'));
+                }
+
+                $limiter->registerAttempt();
+
+                $user = Admin::instance()->users()->get($data->get('username'));
+
+                // Authenticate user
+                if ($user !== null && $user->authenticate($data->get('password'))) {
+                    Session::set('FORMWORK_USERNAME', $data->get('username'));
+
+                    // Regenerate CSRF token
+                    CSRFToken::generate();
+
+                    $time = $this->log('access')->log($data->get('username'));
+                    $this->registry('lastAccess')->set($data->get('username'), $time);
+
+                    $limiter->resetAttempts();
+
+                    if (($destination = Session::get('FORMWORK_REDIRECT_TO')) !== null) {
+                        Session::remove('FORMWORK_REDIRECT_TO');
+                        $this->redirect($destination);
+                    }
+
+                    $this->redirectToPanel();
+                    //$this->redirect('/user');
+                }
+
+                $this->error($this->label('login.attempt.failed'), [
+                    'username' => $data->get('username'),
+                    'error'    => true
+                ]);
+
+                break;
+        }
+    }
+
+    /**
      * Authentication@logout action
      */
     public function logout(): void
